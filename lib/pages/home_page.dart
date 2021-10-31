f362414c80494467fa078d865f2bc1e3e8b18407
Devansh_Sahani <sahanidev21@gmail.com>
2021-11-01 04:08:52 +0530

    Auth

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,130 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        leading: Icon(Icons.book),
+        title: Text("BDCoE Blogs"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10.0,vertical:2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("AI/ML",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
+                    Column(
+                      children: [
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Explore",style: TextStyle(color: Colors.white),)),
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Create",style: TextStyle(color: Colors.white))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              color: Colors.blue[400],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10.0,vertical:2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Web Dev",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
+                    Column(
+                      children: [
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Explore",style: TextStyle(color: Colors.white),)),
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Create",style: TextStyle(color: Colors.white))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              color: Colors.blue[400],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10.0,vertical:2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("App Dev",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
+                    Column(
+                      children: [
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Explore",style: TextStyle(color: Colors.white),)),
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Create",style: TextStyle(color: Colors.white))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              color: Colors.blue[400],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10.0,vertical:2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Designing",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
+                    Column(
+                      children: [
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Explore",style: TextStyle(color: Colors.white),)),
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Create",style: TextStyle(color: Colors.white))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              color: Colors.blue[400],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:10.0,vertical:2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Miscellaneous",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
+                    Column(
+                      children: [
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Explore",style: TextStyle(color: Colors.white),)),
+                        TextButton(style: TextButton.styleFrom(backgroundColor: Colors.blue[300]),onPressed: (){}, child: Text("Create",style: TextStyle(color: Colors.white))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              color: Colors.blue[400],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
